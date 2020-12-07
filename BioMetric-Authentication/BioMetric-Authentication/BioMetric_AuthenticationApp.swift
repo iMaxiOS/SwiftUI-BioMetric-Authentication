e7427eec5a3005abe2c4a70f159b34ea0d97f406
Macostik <y.granchenko@gmail.com>
2020-12-07 11:14:59 +0200

    add project

---
+++ BioMetric-Authentication/BioMetric-Authentication/BioMetric_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioMetric_AuthenticationApp.swift
+//  BioMetric-Authentication
+//
+//  Created by Maxim Granchenko on 07.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct BioMetric_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
